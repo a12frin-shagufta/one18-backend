b5157e197cb212f909e3a8b75aa905ba2890569e
Shagufta Afrin <cloudname99@gmail.com>
2026-01-12 21:27:12 +0530

    ff

--- utils/delivery.js
+++ utils/delivery.js
@@ -1,7 +1,12 @@
 export const FAR_PREFIXES = [
+  // EAST
+  "46","47","48","49","50","51","52","53","54","55","56","57",
+
+  // FAR NORTH / WEST
   "60","61","62","63","64","65","66","67","68","69",
   "70","71","72","73","74","75","76","77","78","79"
 ];
+
 
 export function calculateDeliveryFee({ postalCode, subtotal }) {
   if (!postalCode) return null;
