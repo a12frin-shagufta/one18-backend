109960555887a4636c0c215b492196cf0cc27c70
Shagufta Afrin <cloudname99@gmail.com>
2026-01-25 19:56:19 +0530

    ss

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
+
 
 // routes
 import menuRoutes from "./routes/menuRoutes.js";
