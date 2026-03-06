790011f22fab2bbe0d75cd46d94d9c2e2678c041
Shagufta Afrin <cloudname99@gmail.com>
2026-03-06 16:09:10 +0530

    s

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -201,7 +201,7 @@ if (req.files && req.files.length > 0) {
       isBestSeller: isBestSeller === "true",
       stock: stockValue,
 
-      isAvailable: isAvailable !== "false",
+      isAvailable: typeof isAvailable === "boolean" ? isAvailable : item.isAvailable,
       images: finalImages,
     };
 
